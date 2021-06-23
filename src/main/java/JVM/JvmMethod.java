cdb14ff56fcbb26e5d9ecd21a0f13ce519c63392
Mathias Jakobsen <m.jakobsen.1@research.gla.ac.uk>
2021-06-23 11:36:00 +0100

    Add access permission flags to JvmMethod

--- src/main/java/JVM/JvmMethod.java
+++ src/main/java/JVM/JvmMethod.java
@@ -23,7 +23,9 @@ import CFG.InstructionGraph;
 import JVM.Instructions.JvmInstruction;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class JvmMethod {
 
     int access;
@@ -34,6 +36,10 @@ public class JvmMethod {
 
     public JvmMethod(int access, String name, String descriptor, String signature) {
         this(access, name, descriptor, signature, new ArrayList<>());
+    }
+
+    public boolean is(int perm) {
+        return (access & perm) == perm;
     }
 
     public JvmMethod(
@@ -116,5 +122,20 @@ public class JvmMethod {
                 + ", instructions=\n"
                 + str
                 + '}';
+    }
+
+    public static final class AccessFlags {
+        public static final int ACC_PUBLIC = 0x0001;
+        public static final int ACC_PRIVATE = 0x0002;
+        public static final int ACC_PROTECTED = 0x0004;
+        public static final int ACC_STATIC = 0x0009;
+        public static final int ACC_FINAL = 0x0010;
+        public static final int ACC_SYNCHRONIZED = 0x0020;
+        public static final int ACC_BRIDGE = 0x0040;
+        public static final int ACC_VARARGS = 0x0080;
+        public static final int ACC_NATIVE = 0x0100;
+        public static final int ACC_ABSTRACT = 0x0400;
+        public static final int ACC_STRICT = 0x0800;
+        public static final int ACC_SYNTHETIC = 0x1000;
     }
 }
