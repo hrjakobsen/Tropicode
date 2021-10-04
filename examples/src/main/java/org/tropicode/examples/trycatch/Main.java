98929369eb81d90e2056dee307da9cc7a30c79c5
Mathias Jakobsen <m.jakobsen.1@research.gla.ac.uk>
2021-10-04 11:33:49 +0100

    Make try-catch example more interesting by adding object with a protocol

--- examples/src/main/java/org/tropicode/examples/trycatch/Main.java
+++ examples/src/main/java/org/tropicode/examples/trycatch/Main.java
@@ -6,12 +6,27 @@
 
 package org.tropicode.examples.trycatch;
 
+import org.tropicode.checker.annotations.Protocol;
+
+@Protocol("{doMethodCall; {nextMethod; {finalMethod; end}}}")
 public class Main {
+    @Protocol("{doMethodCall; {nextMethod; {finalMethod; end}}}")
+    private static class ClassWithProtocol {
+        public void doMethodCall() {}
+
+        public void nextMethod() {}
+
+        public void finalMethod() {}
+    }
+
     public static void main(String[] args) {
+        ClassWithProtocol obj = new ClassWithProtocol();
         try {
+            obj.doMethodCall();
             int a = 1 / 0;
         } catch (ArithmeticException e) {
             System.out.println("Error");
         }
+        obj.nextMethod();
     }
 }
